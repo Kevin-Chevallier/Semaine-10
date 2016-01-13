405fce6ef8f8cd6aafb06b6c5ff7ed9007340cbc
Kevin Chevallier <chevallier.kevin@gmail.com>
2016-01-13 13:24:31 +0100

    Create stylesheet.css

---
+++ CSS/stylesheet.css
@@ -0,0 +1,38 @@
+#en_tete img{
+    width:60%;   
+    height:30%;
+    display:block;
+    margin:auto;
+}
+body {
+    font-family: 'Gothic', sans-serif;
+    font-weight: 200;
+    font-size: 16px;
+    text-align: justify;
+    position: relative;
+    background: #B8B8B8;
+    width: 100%;
+    margin: 40px;
+    overflow-x: hidden;
+}
+.container {
+    position: relative;
+    width: 600px;
+    margin: 0 auto;
+    color: #575e72;
+    cursor: crosshair;
+    padding: 30px;
+    text-align: center;
+}
+bouton {
+    width: 250px;
+    height: 60px;
+    padding: 0;
+    margin: 10px 10px;
+    background: #4AC4FF;
+    border: 10;
+    cursor: crosshair;
+    font-size: 15px;
+    line-height: 15px;
+    text-align: center;
+}
